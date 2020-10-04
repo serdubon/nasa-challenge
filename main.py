7eb15b4462baf22d79ea6b59d14968dea33a6b22
Wences <villegaswences@gmail.com>
2020-10-04 13:50:38 -0300

    Update Uruguaiana

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import pydeck as pdk
 from PIL import Image
 import datetime
 import pandas as pd
+import time 
+from os.path import join
 
 cordinates = {
-    "Costa Rica": [9.939546, -84.095995],
-    "Argentina": [-32.898482, -68.820795],
+    "Uruguaiana": [-29.75472, -57.08833],
+    "Fortaleza": [-3.71722 -38.54306],
 }
 
 
@@ -16,8 +18,8 @@ def main():
     st.markdown("---")
     st.header("Satellite Images")
 
-    option = st.selectbox("Select a Country", ("Costa Rica", "Argentina"))
-    date = st.date_input("Date of the image", datetime.date.today())
+    option = st.selectbox("Select a Location", ("Uruguaiana", "Fortaleza"))
+    date = st.date_input("Date of the image", datetime.date.fromisoformat("2019-01-07"))
 
     st.pydeck_chart(
         pdk.Deck(
@@ -30,9 +32,15 @@ def main():
             },
         )
     )
-
+    with st.spinner('Processing Sentinel-1 Data'):
+        time.sleep(3)
+    st.success('Processing complete!')
     name_country = option.replace(" ", "_").lower()
-    image = Image.open("images/{}.png".format(name_country))
+    try:
+        image = Image.open("images/{}-{}.jpg".format(name_country, date))
+    except Exception:
+        image = Image.open(join('images', 'no_data.jpeg'))
+    
     st.image(image, width=700)
     st.markdown("---")
 
@@ -40,9 +48,15 @@ def main():
 
     data = load_data()
     data_slice = data[(data["country"] == option) & (data["date"].dt.date == date)]
-    st.dataframe(data_slice)
 
-@st.cache(persist=True)
+    st.dataframe(data_slice)
+    st.markdown("---")
+
+    st.header("Cost calculation")
+    st.info('')
+
+
+@st.cache(persist=False, ttl=10)
 def load_data():
     data = pd.read_csv("./data/dataset.csv",)
     lowercase = lambda x: str(x).lower()
